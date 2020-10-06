2ef3e833c177d917b93125fa688fc72af454f616
johnlafata <johnlafata61@gmail.com>
2020-10-06 06:21:16 -0400

    removed user & pw

--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-  localUrl: 'mongodb://root:LeBqBhv1FO@mongodb.node-todo.svc.cluster.local/meanstacktutorials'
+  localUrl: 'mongodb://mongodb.node-todo.svc.cluster.local/meanstacktutorials'
 };
