9a60dc523729529734938bf5078ada45fa7f001d
johnlafata <johnlafata61@gmail.com>
2020-10-06 06:16:18 -0400

    added pw in code... bad practice

--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-  localUrl: 'mongodb://mongodb.node-todo.svc.cluster.local/meanstacktutorials'
+  localUrl: 'mongodb://root:LeBqBhv1FO@mongodb.node-todo.svc.cluster.local/meanstacktutorials'
 };
