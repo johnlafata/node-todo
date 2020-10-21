b4e2282d346347a57b1075d9bc99faf3d567fd82
John LaFata <jlafata@pivotal.io>
2020-10-21 05:52:46 -0400

    added cfenv

--- config/database.js
+++ config/database.js
@@ -1,3 +1,19 @@
+var cfenv = require("cfenv")
+var appEnv = cfenv.getAppEnv()
+
+var service_url=appEnv.getServiceURL("mongodb2", {
+      pathname: "database",
+      auth:     ["username", "password"]
+    })
+
+// var service_url = appEnv.getService("mongodb2").credentials.uri;
+if ( service_url == null ){
+    service_url = 'mongodb://mongodb'
+}
+
 module.exports = {
-  localUrl: 'mongodb://mongodb.node-todo.svc.cluster.local/meanstacktutorials'
+  localUrl:
+      service_url+"/meanstacktutorials"
+  //localUrl: 'mongodb://mongodb/meanstacktutorials'
+  //localUrl: 'mongodb://mongodb.node-todo.svc.cluster.local/meanstacktutorials'
 };
