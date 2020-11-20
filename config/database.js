0b80b845616271ade755b6746e3100c31ff052dd
John LaFata <jlafata@pivotal.io>
2020-11-20 12:02:48 -0500

    updates config/database.js to use environment variables & adds service to cf manifest.yml

--- config/database.js
+++ config/database.js
@@ -1,20 +1,39 @@
 var cfenv = require("cfenv")
 var appEnv = cfenv.getAppEnv()
 
-var service_url=appEnv.getServiceURL("mongodb2", {
+var cf_service_url=appEnv.getServiceURL("mongodb2", {
       pathname: "database",
       auth:     ["username", "password"]
     })
 
 // var service_url = appEnv.getService("mongodb2").credentials.uri;
-if ( service_url == null ){
-    service_url = 'mongodb://mongodb';
+if ( cf_service_url == null ){
+    cf_service_url = 'mongodb://mongodb';
 }
-console.log(">>> service_url: " + service_url);
+
+var node_service_host=process.env.MONGO_DATABASE_HOST;
+var node_service_root_password=process.env.MONGODB_ROOT_PASSWORD
+
+// get environment variables from k8s deployment
+var mongo_db_url=null;
+if (node_service_host !=null && node_service_root_password!=null){
+    mongo_db_url="mongodb://root:"+node_service_root_password+"@"+node_service_host+":27017";
+}
+// if url still empty, get environment variables from k8s deployment
+if (mongo_db_url ==null && cf_service_url!=null){
+    mongo_db_url=cf_service_url;
+}
+
+console.log(">>> service_url: " + cf_service_url);
+console.log(">>> node_service_host: " + node_service_host);
+console.log(">>> node_service_root_password: " + node_service_root_password);
+console.log(">>> mongo_db_url: " + mongo_db_url);
 
 module.exports = {
-  localUrl:
-      service_url+"/meanstacktutorials"
-  //localUrl: 'mongodb://mongodb/meanstacktutorials'
-  //localUrl: 'mongodb://mongodb.node-todo.svc.cluster.local/meanstacktutorials'
+    localUrl:
+        mongo_db_url
+    //    "mongodb://root:4vK50Fjsck@192.168.86.228:27017"
+    //    service_url+"/meanstacktutorials"
+    //localUrl: 'mongodb://mongodb/meanstacktutorials'
+    //localUrl: 'mongodb://mongodb.node-todo.svc.cluster.local/meanstacktutorials'
 };
